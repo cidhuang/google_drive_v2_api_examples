d9636504eeca54aec555d868c08799182f58a603
Damon Douglas <douglas.damon@gmail.com>
2013-02-09 15:02:21 -0500

    commit uupdate_file_example.dart

--- console/files/update_file_example.dart
+++ console/files/update_file_example.dart
@@ -1,6 +1,5 @@
 // Run as 
-// dart console/files/patch_file_example.dart
-
+// dart console/files/update_file_example.dart
 
 import "dart:io";
 import "dart:async";
@@ -8,19 +7,19 @@ import "dart:json" as JSON;
 import "package:google_oauth2_client/google_oauth2_console.dart";
 import "package:google_drive_v2_api/drive_v2_api_console.dart" as drivelib;
 import "package:http/http.dart" as http;
+import "dart:crypto";
 
-void patchFile(String fileId, Map newMetaData, drivelib.Drive drive, Function callback) {
+void updateFile(String fileId, String base64content, drivelib.Drive drive, Function callback) {
   
-  drivelib.File file = new drivelib.File.fromJson(newMetaData);
-  
-  drive.files.patch(file,fileId).then((drivelib.File patchedFile){
-    Function.apply(callback,[patchedFile]);
+  var request = drive.files.get(fileId).then((drivelib.File rtrvdFile){
+    drive.files.update(rtrvdFile, fileId, content:base64content).then((drivelib.File updatedFile){
+      Function.apply(callback,[updatedFile]);
+    });
   });
-  
     
 }
 
-void onPatchFile(drivelib.File file) {
+void onUpdateFile(drivelib.File file) {
   print(file);
 }
 
@@ -34,10 +33,13 @@ void run(Map client_secrets) {
   var drive = new drivelib.Drive(auth);
   drive.makeAuthRequests = true;
   
-  Map newMetaData = {
-                     "title":"Just Changed Title Again"
-  };
-  patchFile("180oQjByG0c2U2vxD1ujyIyJ0fLu7yEQ-kXIHavCbLBo", newMetaData, drive, onPatchFile);
+  String content = "All work and no play makes Jack a dull boy";
+  
+  ListOutputStream los = new ListOutputStream();
+  los.writeString(content);
+  String base64content = CryptoUtils.bytesToBase64(los.read());
+  
+  updateFile("180oQjByG0c2U2vxD1ujyIyJ0fLu7yEQ-kXIHavCbLBo", base64content, drive, onUpdateFile);
 }
 
 void main() {
@@ -54,7 +56,4 @@ void main() {
   });
  
 }
-
-
-
 
